f777b541601f70f9fde63d235ad2a3f84267450c
Daan Rosbergen <d.rosbergen@gmail.com>
2015-11-25 08:02:21 +0100

    Add comments to Tweet model

--- iscp/Model/Tweet.py
+++ iscp/Model/Tweet.py
@@ -3,32 +3,39 @@ class Tweet(object):
 	datetime = 0
 	user = ""
 
-	"""docstring for Tweet"""
 	def __init__(self, tweet, datetime, user):
+		"""Creates a Tweet object containing a tweet."""
 		super(Tweet, self).__init__()
 		self.tweet = tweet
 		self.datetime = datetime
 		self.user = user
 
 	def getTweet(self):
+		"""Return the tweet"""
 		return self.tweet
 
 	def getDate(self):
+		"""Return the a datetime stamp of the tweet"""
 		return self.datetime
 
 	def getUser(self):
+		"""Return user of the tweet"""
 		return self.user
 		
 	def setTweet(self, tweet):
+		"""Set the tweet"""
 		self.tweet = tweet
 
 	def setDate(self, date):
+		"""Set the datetime of the tweet"""
 		self.datetime = date
 
 	def setUser(self, user):
+		"""Set the user of the tweet"""
 		self.user = user
 
 	def printTweet(self):
+		"""Utility method used to print the tweet"""
 		print(self.getTweet())
 		print(self.getDate())
 		print(self.getUser())
