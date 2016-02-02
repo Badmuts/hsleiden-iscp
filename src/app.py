8b119acc6f97e17b6252608d887e644652cec938
Daan Rosbergen <d.rosbergen@gmail.com>
2016-02-02 16:57:24 +0100

    Read config data from json file instead of env

--- src/app.py
+++ src/app.py
@@ -1,9 +1,15 @@
 import router
+import json
+
+""" Read the config file"""
+f = open("config.json")
+config = json.loads(f.read())
+f.close()
 
 class App(object):
 	def run(self):
 		route = router.Router()
-		route.run()		
+		route.run()
 
 if __name__ == "__main__":
 	app = App()
