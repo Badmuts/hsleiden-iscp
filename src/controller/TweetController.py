30cf76ba5aa89ad38261a3909eeb7ddf9a0f2ea7
Daan Rosbergen <d.rosbergen@gmail.com>
2016-02-02 17:25:26 +0100

    Remove unused code

--- src/controller/TweetController.py
+++ src/controller/TweetController.py
@@ -5,11 +5,6 @@ import json
 from service.Listener import Listener
 import app
 
-# CONSUMER_KEY 		= os.environ['CONSUMER_KEY']
-# CONSUMER_SECRET 	= os.environ['CONSUMER_SECRET']
-# ACCESS_TOKEN 		= os.environ['ACCESS_TOKEN']
-# ACCESS_TOKEN_SECRET = os.environ['ACCESS_TOKEN_SECRET']
-
 class TweetController(object):
 
 	def __init__(self):		
